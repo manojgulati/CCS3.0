29b7ddeb0f1a94d26239c57b62343feb39487cf5
Manoj Gulati <er.manojgulati@gmail.com>
2016-05-23 16:24:48 -0700

    updated scripts

--- Scripts/NI 6259 Scripts/Realtime_Sensing_Code_ver2/Dump_data.m
+++ Scripts/NI 6259 Scripts/Realtime_Sensing_Code_ver2/Dump_data.m
@@ -4,9 +4,10 @@ data_acq = [event.Data];
 for i=1:num_sensors
 %     disp(i);
     data_filt(:,i) = filter(filt_hand, data_acq(:,i));
-    plot(data_filt(:,i));
+    data_rms(i) = rms(data_filt(:,i)); % compute RMS value of measured signals
+    plot(data_rms(:,i));
 end
-disp(size(data_filt));
+disp(size(data_rms));
 fname = sprintf('./Data/Data_%s.mat',datestr(clock,30));
-save(fname,'data_filt');
+save(fname,'data_rms');
 end
